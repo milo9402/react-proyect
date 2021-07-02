5120cd7ed80c36f1e04db8ef78fc0cd67bb27060
Camilo Castrillon <desarrollo@servitel.co>
2021-07-01 19:01:34 -0500

    initial commit .gitignore

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aqui va a ir un Front!
         </a>
       </header>
     </div>
